b45631a15a8647f5f3dfae1a4e7668781a59f97a
Nabil Yassine <nabil.yassine.dev@gmail.com>
2022-01-27 17:24:39 +0100

    Added animation to bottom of carrousel

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let active = 1;
 const arrows = document.querySelectorAll("svg");
 
+
 const decrement = () => {
-active === 1 ? "" : active -= 1;
+document.querySelector("#pageSelector li:nth-child("+active+")").classList.remove("display");
+active -= 1;
+active === 1 ? arrows[0].classList.add('unclickable-arrow') : arrows[1].classList.remove('unclickable-arrow');
 }
 
+
 const increment = () => {
-active === 3 ? "" : active += 1;
+active += 1;
+document.querySelector("#pageSelector li:nth-child("+active+")").classList.add("display");
+active === 3 ? arrows[1].classList.add('unclickable-arrow') : arrows[0].classList.remove('unclickable-arrow');
 }
+
 
 arrows[0].addEventListener("click", decrement);
 arrows[1].addEventListener("click", increment);
