69f67ed66f9f074f740743b20cb721ec9fa43254
Nabil Yassine <nabil.yassine.dev@gmail.com>
2022-01-28 16:38:48 +0100

    add indent

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const arrows = document.querySelectorAll("svg");
 
 
 const decrement = () => {
-document.querySelector("#pageSelector li:nth-child("+active+")").classList.remove("display");
-active -= 1;
-active === 1 ? arrows[0].classList.add('unclickable-arrow') : arrows[1].classList.remove('unclickable-arrow');
+    document.querySelector("#pageSelector li:nth-child("+active+")").classList.remove("display");
+    active -= 1;
+    active === 1 ? arrows[0].classList.add('unclickable-arrow') : arrows[1].classList.remove('unclickable-arrow');
 }
 
 
 const increment = () => {
-active += 1;
-document.querySelector("#pageSelector li:nth-child("+active+")").classList.add("display");
-active === 3 ? arrows[1].classList.add('unclickable-arrow') : arrows[0].classList.remove('unclickable-arrow');
+    active += 1;
+    document.querySelector("#pageSelector li:nth-child("+active+")").classList.add("display");
+    active === 3 ? arrows[1].classList.add('unclickable-arrow') : arrows[0].classList.remove('unclickable-arrow');
 }
 
 
